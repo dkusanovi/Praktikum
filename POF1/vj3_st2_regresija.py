5c4d8757c4af84d00d635e05a324e2439b4eece7
Dora Kusanovic <dkusanovi@pmfst.hr>
2023-03-04 12:25:06 +0100

    Vjezba 3, ovisnost puta i kvadrata vremena linearnom regresijom.

--- POF1/vj3_st2_regresija.py
+++ POF1/vj3_st2_regresija.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-# nije potrebno radit i lin. regresiju i obicnu metodu
 
+# nije potrebno radit i lin. regresiju i obicnu metodu
 # treba izvuc ovisnost iz formula u skipti
+
 
 # Data for plotting
 x = np.arange(0, 10, 0.01)
